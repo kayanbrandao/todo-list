de077c6662ec926421817490dfa1e0ea4fce6571
kayanbrandao <kayanbrandao.krb@gmail.com>
2022-10-15 14:39:27 -0400

    Atualizando projeto

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const btnAddTask = document.querySelector(".btn-add-task");
+const task = document.querySelector(".task");
+const listTask = document.querySelector(".list-task");
+
+btnAddTask.addEventListener("click", () => {
+	if (task.value) {
+		const li = createLi(task.value);
+		listTask.appendChild(li);
+		task.value = "";
+		return;
+	}
+
+	alert("Campo de tarefa está vazio!");
+});
+
+const createLi = (value) => {
+	const li = document.createElement("li");
+	const btn = createBtnDel();
+	li.setAttribute("class", "li-task")
+	li.innerHTML = value;
+	li.appendChild(btn);
+	return li;
+};
+
+const createBtnDel = () => {
+	const btn = document.createElement("button");
+	btn.setAttribute("class", "btn-del");
+	btn.innerHTML = "Apagar";
+
+	btn.addEventListener("click", () => {
+		btn.parentElement.remove();
+	});
+
+	return btn;
+};
